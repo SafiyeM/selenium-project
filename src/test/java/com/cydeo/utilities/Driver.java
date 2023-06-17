54de596f10b247c9c75b41baba0469c178ab4932
Safiye_Mertcan <safiyemertcan@hotmail.com>
2023-06-17 20:14:21 +0200

    closeDriver method added into Driver.java

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -66,7 +66,17 @@ public class Driver {
     It will use .quit method to quit browsers and then set the driver value back to null.
      */
 
+    public static void closeDriver() {
+        if (driver != null) {
 
+           // This line will terminate the currently existing driver completely. It will not exist going forward.
+            driver.quit();
+            /*
+            We assign the value back to "null" so that my "singleton" can create a newer one if needed.
+             */
+            driver = null;
+        }
+    }
 
 
 }
